8e0a3b55220abf3920f7090cd96add3877026dc4
TienActor <tienmetak@gmail.com>
2024-01-26 22:00:19 +0700

    Bai 33 34 , note chưa hien thị được trang thái tin nhắn

--- lib/auth/chat_screen.dart
+++ lib/auth/chat_screen.dart
@@ -1,6 +1,9 @@
+
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:test_121/main.dart';
 
 import '../models/chat_user.dart';
 
@@ -17,12 +20,72 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        //app Bar
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          flexibleSpace: _appBar(),
-        ),
-      ),
+          //app Bar
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            flexibleSpace: _appBar(),
+          ),
+
+          // hien thi man hinh chat
+          body: Column(
+            children: [
+              Expanded(
+                child: StreamBuilder(
+                  // xử lý dữ liệu từ firebase và hiển thị lên màn hình
+                  //stream: APIs.getAllUsers(),
+                  builder: (context, snapshot) {
+                    switch(snapshot.connectionState){
+
+                      //if data is loading
+                        case ConnectionState.waiting:
+                        case ConnectionState.none:
+                          return const SizedBox();
+
+                        //if some or all data is loaded then show it
+                        case ConnectionState.active:
+                        case ConnectionState.done:
+                         // final data = snapshot.data?.docs;
+                          // _list = data
+                          //         ?.map((e) => Message.fromJson(e.data()))
+                          //         .toList() ??
+                          //     [];
+                    final _list = [];
+
+                    // final data = snapshot.data?.docs;
+                    // _list =
+                    //     data?.map((e) => ChatUser.fromJson(e.data())).toList() ?? [];
+
+                    if (_list.isNotEmpty) {
+                      // Show user list if it is not empty.
+                      return ListView.builder(
+                        itemCount: _list.length,
+                        padding: EdgeInsets.only(top: mq.height * .01),
+                        physics: const BouncingScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          return Text('Messenger: ${_list[index]}');
+                        },
+                      );
+                    } else {
+                      // Show this when the list is empty.
+                      return const Center(
+                        child: Text(
+                          'Nhắn để trò  chuyện !!',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      );
+                    }
+                    }
+
+                  
+                   
+                  },
+                  stream: null,
+                ),
+              ),
+              _chatInput()
+            ],
+          )),
     );
   }
 
@@ -53,24 +116,105 @@ class _ChatScreenState extends State<ChatScreen> {
                   const CircleAvatar(child: Icon(CupertinoIcons.person)),
             ),
           ),
-      
+
           // for adding some space
-          const SizedBox(width: 10,),
+          const SizedBox(
+            width: 10,
+          ),
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
                 widget.user.name,
-                style: const TextStyle(fontSize: 16, color: Colors.black,fontWeight: FontWeight.w500),
+                style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500),
               ),
-              const SizedBox(height: 5,),
-              
-               const Text(
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
                 'Lan cuoi truy cap',
-                style: TextStyle(fontSize: 13, color: Colors.black,fontWeight: FontWeight.w300),
+                style: TextStyle(
+                    fontSize: 13,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w300),
               )
             ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _chatInput() {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          vertical: mq.height * .03, horizontal: mq.width * .03),
+      child: Row(
+        children: [
+          //input text chat and button
+          Expanded(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              child: Row(
+                children: [
+                  // emoji button
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.emoji_emotions,
+                        color: Colors.blueAccent,
+                        size: 26,
+                      )),
+
+                  // chat space
+                  const Expanded(
+                      child: TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    decoration: InputDecoration(
+                        hintText: 'Vui long nhap tin nhan',
+                        hintStyle: TextStyle(color: Colors.blueAccent),
+                        border: InputBorder.none),
+                  )),
+                  // image button
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.image,
+                        color: Colors.blueAccent,
+                        size: 26,
+                      )),
+                  // camera  button
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.camera_alt_rounded,
+                        color: Colors.blueAccent,
+                        size: 26,
+                      ))
+                ],
+              ),
+            ),
+          ),
+
+          //send messenger button
+          MaterialButton(
+            onPressed: () {},
+            minWidth: 0,
+            padding:
+                const EdgeInsets.only(top: 10, bottom: 10, right: 5, left: 10),
+            shape: const CircleBorder(),
+            color: Colors.lightGreen,
+            child: const Icon(
+              Icons.send,
+              color: Colors.blueGrey,
+              size: 29,
+            ),
           )
         ],
       ),
